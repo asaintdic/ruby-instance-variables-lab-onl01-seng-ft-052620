bc3e80c9166757eed9b2aaf16b16b0294196d247
Alain Saint-Dic <alains214@gmail.com>
2020-06-15 02:01:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,11 @@
-class Dog
-  def name=(dog_name)
-    @this_dogs_name =
-dog_name
-  end 
-
-  def name 
+class Dog 
+  
+  def name=(name)
+    @this_dogs_name = name
+  end
+  
+  def name
     @this_dogs_name
-  end 
+  end
 end 
-
-lassie = Dog.new
-lassie.name = "Lassie"
- 
-lassie.name
     
